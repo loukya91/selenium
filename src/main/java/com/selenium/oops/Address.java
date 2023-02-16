b92aba55c47ce9191872d0551de0e6648b51c589
SRINIVASA KUDUTHINI <srinivasagk910@gmail.com>
2023-02-16 10:52:53 -0800

    add one java class

---
+++ src/main/java/com/selenium/oops/Address.java
@@ -0,0 +1,26 @@
+package com.selenium.oops;
+
+public class Address {
+    //data members
+    String name;
+    String stname;
+    int aptno;
+    String city;
+    int zipcode;
+
+    // method to print address
+    void printAddress(){
+       String name="loukya";
+
+
+
+    }
+    public static void main(){
+        System.out.println(name);
+        System.out.println(stname);
+        System.out.println(aptno);
+        System.out.println(city);
+        System.out.println(zipcode);
+    }
+
+}
